4f081d684270bb095f5623f344933c5df55dce9f
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-25 11:34:40 +0200

    Update and rename install_os.sh to install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
+
+#Initialisation et installation
+
+#Récupération des données
+git clone https://github.com/vttotor98/genesis_files/
+#Installation
+genesis_files/install.sh
