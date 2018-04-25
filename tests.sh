6e43492a1898d55ded8ef64335f7134107e23cb1
vttotor98 <victor.tirebaque.988@gmail.com>
2018-04-25 16:07:32 +0200

    modif de tests.sh

--- tests.sh
+++ tests.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
-docker images
-read img
-docker rmi -f $img
 docker rmi -f ubuntu
 rm -rf scripts
 git clone https://github.com/vttotor98/scripts
-docker build -f scripts/init_docker scripts/
