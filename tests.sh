ffb49e497f31d3cf98afbed2c4dc6c411586af7f
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-04-26 10:10:45 +0200

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
+docker images
+read var
+docker rmi -f $var
 docker rmi -f ubuntu
 rm -rf scripts
 git clone https://github.com/vttotor98/scripts
