bf1958b79f46edf3ee8db2a1364ad0736d53a600
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-03 10:21:27 +0200

    Delete initialisation_demarrage.sh

--- initialisation_demarrage.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Author Victor TIREBAQUE for Agaetis Clermont-Ferrand
-
-geth --identity node1 init ~/blockchain/genesis_folder/firstGenesis.json --datadir ~/blockchain/.ethereum_private/ACPrivateChain
-chmod +r geth.log
-nohup geth --datadir ~/blockchain/.ethereum_private/ACPrivateChain --networkid 988 --nodiscover &>geth.log &
