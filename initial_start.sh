20844bf04d8369ec1831fccd6c375995813d59ed
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-03 16:43:21 +0200

    Update initial_start.sh

--- initial_start.sh
+++ initial_start.sh
@@ -8,7 +8,7 @@ if [ ! -s $fic ]; then
   docker build -f ~/scripts/init_docker . >$fic_out
   #Nous récoupérons l'id de l'images docker qui se trouve à la fin du fichier out_install.log
   dock_id=`tail -n 1 $fic_out | cut -d ' ' -f 3`
-  mv ~/scripts/start.sh
+  mv ~/scripts/start.sh /
   echo "docker run -it $dock_id bash" >>$fic
   chmod +x $fic
 else
