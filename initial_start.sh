9a8c087ae709a9fb18fb978ee2fb33ca6c230590
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-03 15:28:50 +0200

    Update initial_start.sh

--- initial_start.sh
+++ initial_start.sh
@@ -6,9 +6,11 @@ fic_out=out.txt
 #Est-ce que le fichier start existe, et est-il vide
 if [ ! -s $fichier ]; then
   docker build -f ~/scripts/init_docker . >$fic_out
+  #Nous récoupérons l'id de l'images docker   
   dock_id=tail -n 1 $fic_out | cut -d ' ' -f 3
-  echo "docker run -it $dock_id bash" >/start.sh
+  mv ~/scripts/start.sh
+  echo "docker run -it $dock_id bash" >>/start.sh
   chmod +x /start.sh
 else
-  sh /start.sh
+  /start.sh
 fi
