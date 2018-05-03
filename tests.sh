cf8170cb9c7ad0d02b7230afcfea7ca0a1414617
vttotor98 <33938785+vttotor98@users.noreply.github.com>
2018-05-03 10:03:10 +0200

    Update tests.sh

--- tests.sh
+++ tests.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 #Author : Victor TIREBAQUE for Agaetis Clermont-Ferrand
 
-#scripts afin de supprimer les img docker que l'on veut pour repartir de 0
+#Scripts afin de supprimer les img docker que l'on veut pour repartir de 0
+#Choix de l'image
 docker images
 read var
+#Suppression de l'image
 docker rmi -f $var
+#Suppression de scripts pour mettre à jour ou le télécharger pour la première
 rm -rf scripts
+
 git clone https://github.com/vttotor98/scripts
 docker images
 docker build -f ~/scripts/init_docker .
