44405d4828ebb1858313983bb1ed74f3e8e2b272
root <victor.tirebaque.988@gmail.com>
2018-05-30 13:13:54 +0000

    corrections des paths

--- init_docker.sh
+++ init_docker.sh
@@ -10,14 +10,15 @@ fic_out=out_install.log
 if [ ! -s $fic ]; then
 	
 	#exécution du dockerfile
-	docker build -f ~/scripts/init_docker/init_docker.sh . >$fic_out
+	docker build -f ~/scripts/files/init_docker . >$fic_out
 	
 	#Nous récoupérons l'id de l'images docker qui se trouve à la fin du fichier out_install.log
 	dock_id=`tail -n 1 $fic_out | cut -d ' ' -f 3`
 	
 	#Déplacement du fichier pour plus de confort d'utilisation
-	mv ~/scripts/start.sh ~/
+	#mv ~/scripts/start.sh ~/
 	echo "docker run -it $dock_id bash" >>$fic
+	cp ~/scripts/start.sh ~/
 else
 	$fic
 fi
